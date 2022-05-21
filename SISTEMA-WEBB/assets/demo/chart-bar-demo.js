b4d71d1b7e74a35181b15dda0e15e99e6683fbe8
unknown <cpablop1@miumg.edu.gt>
2022-05-20 20:50:22 -0600

    Files upload

---
+++ SISTEMA-WEBB/assets/demo/chart-bar-demo.js
@@ -0,0 +1,87 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Bar Chart Example
+var ctx = document.getElementById("myBarChart");
+var myLineChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"],
+    datasets: [{
+      label: "Revenue",
+      backgroundColor: "#2ec4b6",
+      borderColor: "rgba(2,117,216,1)",
+      data: [5000, 2500, 3000, 3500, 9352, 10984],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 15000,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          display: true
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+var ctx = document.getElementById("myBarChart2");
+var myLineChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["Enero", "Febrero", "Marzo", "April", "Mayo", "Junio"],
+    datasets: [{
+      label: "Revenue",
+      backgroundColor: "#1a759f",
+      borderColor: "1a759f",
+      data: [6000, 5312, 6251, 1000, 3500, 14984],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 15000,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          display: true
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
